806c9c71951ec5230de5bd6f4f13717c0566c604
VasylArtymovych <art777vasss@gmail.com>
2022-06-07 19:15:10 +0300

    update App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import { nanoid } from 'nanoid';
 import ContactForm from "components/ContactForm";
 import ContactList from "components/ContactLIst";
 import Filter from "components/Filter";
