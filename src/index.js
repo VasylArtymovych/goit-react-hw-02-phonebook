af71e24263fc7397dc7146b3677a8468650562e7
VasylArtymovych <art777vasss@gmail.com>
2022-06-27 13:09:24 +0300

    update gkobal style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import styled, {createGlobalStyle} from "styled-components";
+import {createGlobalStyle} from "styled-components";
 import  App  from 'components/App';
 import './index.css';
 
 const Global = createGlobalStyle`
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
 `;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
